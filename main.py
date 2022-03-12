2320343923f13c613cc264d68c234cd45779229b
benji-yang <77566221+benji-yang@users.noreply.github.com>
2022-03-12 13:51:39 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+#%%
+#Import relavent data
+import pandas as pd
+import numpy as np
+import datetime as dt
+import matplotlib.pyplot as plt
+
+#%%
+# Load data
+epex_data = pd.read_csv('epex_day_ahead_price.csv')
+spot_data = pd.read_csv('spot_intraday_price.csv')
+system_data = pd.read_csv('systemprice.csv')
+
+#%%
+# Analyzed data
+print(epex_data.head(5))
+print(spot_data.head(5))
+print(system_data.head(5))
+#%%
+# Plot data
+plt.plot(epex_data['timestamp'], epex_data['apx_da_hourly'])
+plt.show()
+# %%
